d0708b2539a063d0008d3f0cde31de679f8376fc
HiagoSD <hiago_silva_duarte@hotmail.com>
2025-03-27 17:09:32 -0300

    feat: :sparkles: Created delete/list/update methods

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -4,6 +4,10 @@ import UserController from '../controllers/UserController';
 const router = Router();
 
 router.post('/', UserController.createUser);
-router.get('/:email', UserController.getUserEmail);
+router.get('/email/:email', UserController.getUserEmail);
+router.get('/id/:id', UserController.findById)
+router.get('/', UserController.listUsers)
+router.put('/:id', UserController.updateUserById)
+router.delete('/:id', UserController.deleteUserById)
 
 export default router;
