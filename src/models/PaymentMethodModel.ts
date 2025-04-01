fc1af0b64ccf8e872cd8037d627ae78a942f4baa
HiagoSD <hiago_silva_duarte@hotmail.com>
2025-04-01 13:04:50 -0300

    feat: :sparkles: Create payment method model.

--- src/models/PaymentMethodModel.ts
+++ src/models/PaymentMethodModel.ts
@@ -8,23 +8,40 @@ export enum PaymentMethod {
   INVOICE = "INVOICE"
 }
 
+export interface Card extends Document {
+  cardLastFour: string;
+  cardBrand: string;
+  expirationDate: string;
+  cardName: string;
+}
+
+const CardSchema = new Schema<Card>(
+  {
+    cardLastFour: { type: String, required: true },
+    cardBrand: { type: String, required: true },
+    expirationDate: { type: String, required: true },
+    cardName: { type: String, required: true }
+  }, 
+  { timestamps: true, versionKey: false }
+)
+
 export interface IPaymentMethod extends Document {
   companyId: mongoose.Schema.Types.ObjectId;
-  amount: number;
-  date: Date;
+  paymentDeadLine: Date;
   paymentMethod: PaymentMethod;
+  cards: Card[];
 }
 
 const PaymentMethodSchema = new Schema<IPaymentMethod>(
   {
     companyId: { type: mongoose.Schema.Types.ObjectId, ref: "Company", required: true },
-    amount: { type: Number, ref: "Payment", required: true },
-    date: { type: Date, required: true },
+    paymentDeadLine: { type: Date, required: true },
     paymentMethod: {
       type: String,
       enum: Object.values(PaymentMethod),
-      default: PaymentMethod.PIX
-    }
+      default: PaymentMethod.CREDITCARD
+    },
+    cards: { type: [CardSchema], default: [] }
   },
   { timestamps: true, versionKey: false }
 
