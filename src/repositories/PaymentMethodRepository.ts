685b116c968670dc37b56e528cf2f8e57238bd14
HiagoSD <hiago_silva_duarte@hotmail.com>
2025-04-01 22:29:02 -0300

    feat: :sparkles: Create payment repository.

---
+++ src/repositories/PaymentMethodRepository.ts
@@ -0,0 +1,21 @@
+import PaymentMethodModel, { IPaymentMethod } from "@models/PaymentMethodModel";
+
+class PaymentMethodRepository {
+  async createPaymentMethod(data: IPaymentMethod): Promise<IPaymentMethod> {
+    return await PaymentMethodModel.create(data);
+  }
+
+  async findPaymentMethodById(_id: string): Promise<IPaymentMethod | null> {
+    return await PaymentMethodModel.findById({ _id });
+  }
+
+  async updatePaymentMethodById(_id: string, data: Partial<IPaymentMethod>): Promise<IPaymentMethod | null> {
+    return await PaymentMethodModel.findByIdAndUpdate({ _id }, data, { new: true });
+  }
+
+  async deletePaymentMethodById(_id: string): Promise<IPaymentMethod | null> {
+    return await PaymentMethodModel.findByIdAndDelete({ _id });
+  }
+}
+
+export default new PaymentMethodRepository();
