beffc20fef497924a68cd8136263aee6685a87cf
HiagoSD <hiago_silva_duarte@hotmail.com>
2025-04-03 14:57:56 -0300

    feat: :sparkles: Close routers with auth middleware and fix errors.

--- src/controllers/PaymentMethodController.ts
+++ src/controllers/PaymentMethodController.ts
@@ -1,49 +1,47 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import PaymentMethodService from "@services/PaymentMethodService";
 import ApiResponseHandler from "@utils/ApiResponseHandler";
 
-class PaymentMethodController{
-    async createPaymentMethod(req: Request, res: Response, next: NextFunction){
-        try{
+class PaymentMethodController {
+    async createPaymentMethod(req: Request, res: Response, next: NextFunction) {
+        try {
             const newPaymentMethod = await PaymentMethodService.createPaymentMethod(req.body);
             ApiResponseHandler.success(res, newPaymentMethod, 201);
-        } catch (error){
+        } catch (error) {
             next(error);
         }
     }
 
     //metodo para identificar o tipo de pagamento?
-    async findPaymentMethodById(req: Request, res: Response, next: NextFunction){
-        try{
-            const {id} = req.params;
+    async findPaymentMethodById(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id } = req.params;
             const paymentMethod = await PaymentMethodService.findPaymentMethodById(id);
             ApiResponseHandler.success(res, paymentMethod);
-        } catch (error){
+        } catch (error) {
             next(error);
         }
     }
 
-    async updatePaymentMethodById(req: Request, res: Response, next: NextFunction){
-        try{
-            const {id} = req.params;
-            const paymentMethod = req.body.paymentMethod;
-            const payment = await PaymentMethodService.updatePaymentMethodById(id, paymentMethod);
+    async updatePaymentMethodById(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id } = req.params;
+            const payment = await PaymentMethodService.updatePaymentMethodById(id, req.body);
             ApiResponseHandler.success(res, payment);
-        } catch (error){
+        } catch (error) {
             next(error);
         }
     }
 
-    async deletePaymentMethodById(req: Request, res: Response, next: NextFunction){
-        try{
-            const {id} = req.params;
+    async deletePaymentMethodById(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id } = req.params;
             const payment = await PaymentMethodService.deletePaymentMethodById(id);
             ApiResponseHandler.success(res, payment);
-        } catch (error){
+        } catch (error) {
             next(error);
         }
     }
 }
 
 export default new PaymentMethodController();
-    
--- src/models/CompanyModel.ts
+++ src/models/CompanyModel.ts
@@ -3,12 +3,14 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface ICompany extends Document {
   name: string;
   ownerId: mongoose.Types.ObjectId;
+  CNPJ: string;
 }
 
 const CompanySchema = new Schema<ICompany>(
   {
     name: { type: String, required: true },
     ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true  },
+    CNPJ: { type: String, required: true },   
   },
   { timestamps: true, versionKey: false }
 );
