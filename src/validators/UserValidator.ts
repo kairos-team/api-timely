64a7236097c2f3d30b8d06ac5a39b4a55c1274ba
HiagoSD <hiago_silva_duarte@hotmail.com>
2025-04-09 22:16:23 -0300

    feat: :sparkles: Create payment method validator.

--- src/validators/UserValidator.ts
+++ src/validators/UserValidator.ts
@@ -5,7 +5,7 @@ import { z } from 'zod';
 class UserValidator extends BaseValidator<IUser> {
   getRules() {
     return {
-      name: z.string().min(3, { message: 'O nome deve ter pelo menos 3 caracteres' }),
+      name: z.string().min(2, { message: 'O nome deve ter pelo menos 3 caracteres' }),
       email: z.string().email({ message: 'Email inválido' }),
       phone: z.string().optional(),
       password: z.string().optional(),
