7d9839e3a922f96e521cb0d1f69b01ecf8295e77
Seu Nome <seuemail@dominio.com>
2025-04-09 21:01:45 -0300

    feat: :sparkles:

---
+++ src/validators/CompanyValidator.ts
@@ -0,0 +1,14 @@
+import { ICompany } from "@models/CompanyModel";
+import BaseValidator from "./BaseValidator";
+import { z } from 'zod';
+
+class CompanyValidator extends BaseValidator<ICompany> {
+    getRules() {
+        return {
+            name: z.string().min(3, { message: 'O nome deve ter pelo menos 3 caracteres' }),
+            CNPJ: z.string().regex(/^\d{14}$/, { message: 'CNPJ deve conter exatamente 14 dígitos numéricos' })
+        };
+    }
+}
+
+export default new CompanyValidator();
